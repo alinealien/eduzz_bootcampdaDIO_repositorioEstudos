54bd6ef453520c3498798216296adfba6e7ad014
alinealien <alinebdantunes@gmail.com>
2021-11-02 18:45:01 -0300

    Finalizando aula Default Function Arguments

--- Desenvolvimento Avançado com JavaScript ES6/Funções Avançadas do ES6/defaultfunctionargs.js
+++ Desenvolvimento Avançado com JavaScript ES6/Funções Avançadas do ES6/defaultfunctionargs.js
@@ -2,3 +2,14 @@ function multiply(a, b){
     return a * b;
 }
 console.log(multiply(5, 5));
+
+// Lazzy evaluation
+function randomNumber() {
+    console.log('Generating a tandom number...');
+    return Math.random() * 10;
+}
+function multiply(a, b = randomNumber()) {
+    return a * b;
+}
+console.log(randomNumber(5));
+console.log(randomNumber(5));
