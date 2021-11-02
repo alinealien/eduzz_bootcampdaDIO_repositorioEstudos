96489d5f27420b570d0030b1617cd2087cc92862
alinealien <alinebdantunes@gmail.com>
2021-11-02 18:28:44 -0300

    Default Function Arguments

---
+++ Desenvolvimento Avançado com JavaScript ES6/Funções Avançadas do ES6/arrowfunction.js
@@ -0,0 +1,127 @@
+// Arrow Functions: (=>) Funções anónimas, só pode utiliza-las  ou atribuindo a uma variável ou passando como parâmetro para uma outra função. No JS as funções são de primeira classe.
+
+// EX_1: 
+
+var sun(a,b) => a + b;
+
+console.log(sum(5, 15)); // 20
+
+//se existir  apenas um argumento pode-se omitir os ()
+
+
+
+//EX_objetos literais:
+
+var obj = {
+
+'teste 123'
+
+};
+
+
+
+//Argumento explicito
+
+var createObj = () => ({ test: 123 });
+
+console.log(createObj());
+
+
+
+//Função construtora
+
+function Car(){
+
+this.foo = 'bar';
+
+}
+
+//Arrow functions_EX
+
+var Car = () => {
+
+this.foo = 'bar';
+
+}; //erro. this.
+
+console.log(new Car());
+
+
+
+//Hosting: caracteristicas das functions e das var para serem movidas para o topo do codigo.
+
+log('teste');
+
+function log(value){
+
+console.log(value);
+
+}
+
+//Arrow functions
+
+//não funciona
+
+log('teste');
+
+var log = value => {
+
+console.log(value);
+
+};
+
+//showContext: Vai da um log no contexto
+
+var obj = {
+
+showContext: function showContext(){
+
+console.log('teste');
+
+setTimeout(function){
+
+console.log(this);
+
+}.bind(this),
+
+1000
+
+); //correção de problema de escopo(bind).
+
+},
+
+log function log(value){
+
+console.log(value);
+
+}
+
+};
+
+obj.showContext(); 
+
+//Arrow functions_sem bind
+
+var obj = {
+
+showContext: function showContext(){
+
+// this = obj
+
+setTimeout(() => {
+
+this.log('after 100ms');
+
+}, 1000);
+
+},
+
+log: function log(value){
+
+console.log(value);
+
+}
+
+};
+
+obj.showContext();
