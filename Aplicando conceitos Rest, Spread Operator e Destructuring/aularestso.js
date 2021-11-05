e1c0613709f92ff7ac1216ab08108ad3f3ebc0f5
alinealien <alinebdantunes@gmail.com>
2021-11-04 23:36:15 -0300

    Atualizando arquivo de aula rest e spread operator

--- Aplicando conceitos Rest, Spread Operator e Destructuring/aularestso.js
+++ Aplicando conceitos Rest, Spread Operator e Destructuring/aularestso.js
@@ -20,8 +20,9 @@ function sun(a, b) {
     */
 
     // Exemplo do modo normal de desenvolvimento de função para somar varios parametros
-
-    function sun(a, b) {
+    
+     /* 
+        function sum(a, b) {
         var value = 0;
 
         for (var i =0; i < arguments.length; i++){
@@ -29,7 +30,47 @@ function sun(a, b) {
         }
     
         return value;
+        }
+        console.log(sum(5, 5, 5, 2, 3)); */
+    
+        // Rest Operator ...
+  /*
+     function sun(...args) {
+        console.log(args);
+        console.log(arguments);
     }
         console.log(sun(5, 5, 5, 2, 3));
-    
-        // Rest Operator
+        */
+        /*
+        function sun(...args) {
+            return args.reduce((acc, value) => acc + value, 0);
+        }
+            console.log(sun(5, 5, 5, 2, 3)); */
+
+            //arrow functions
+
+            // erro de referencia nesse ex pq o arguments não foi definido
+            
+            /* const sun = (arguments) => {
+            };
+            console.log(sun(5, 5, 5, 2, 3)); */
+            
+            //para manipular a lista de argumentos neste caso se usa também o rest operator
+           /*
+            const sun = (a, b, ... rest) => { // pega argumentos restantes
+
+            console.log(a, b, rest);
+
+            }; 
+            console.log(sun(5, 5, 5, 2, 3));*/
+
+            const multiply = (...args) => args.reduce((acc, value) => acc + value, 1);
+            
+            const sum = (...rest) => {
+
+                return multiply.apply(undefined, rest);
+            };
+
+            console.log(sum(5, 5, 5, 2, 3));
+
+
