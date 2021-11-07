258ff088aef6b2dc6bc549e5569bd310f881aace
alinealien <alinebdantunes@gmail.com>
2021-11-07 17:13:13 -0300

    Finalizando aula Rest, Spread Operator

--- Desenvolvimento Avançado com JavaScript ES6/Aplicando conceitos Rest, Spread Operator e Destructuring/aularestso.js
+++ Desenvolvimento Avançado com JavaScript ES6/Aplicando conceitos Rest, Spread Operator e Destructuring/aularestso.js
@@ -94,16 +94,52 @@ function sun(a, b) {
             logArgs(...arr); //quebra de array */
 
             // spread operator
-            const str = 'Digital Innovation One';
+            /*const str = 'Digital Innovation One';
             const arr = [1, 2, 3, 4];
             
             function logArgs(a, b, c) {
                 console.log(a, b, c); 
-            }
+            }*/
 
             //const arr2 = arr.concat([5, 6, 7]); //combinar dois arrays
-            const arr2 = [...arr, 5, 6, 7];
+            //const arr2 = [...arr, 5, 6, 7]; //const arr2 = [5, 6, 7, ...arr];
+           //const arr3 = [...arr2, ...arr, 0, 0, 0]; 
+            
+            /*const arrClone = [...arr]; //array literal. clones simples
 
-            console.log(arr2);
+            console.log(arrClone);*/
+            
+           // const arrClone = [...arr];
+
+            const obj = {
+                test: 123,
+                subObj:{
+                    test: 123
+                }
+            };
+
+            //const obj2 = {
+               // ...obj, //o spread não vai fucnionar.(so funciona em objetos iteraveis)
+               // test2: 'hello'
+            //}
+
+             //console.log(obj2);
+
+             /*const obj2 ={
+                 ...obj,
+                 test: 456 // o valor por ultimo prevalece.
+             }; */
+             
+             // const obj2 = obj; //faz referencia
+             const obj2 = {...obj, subObj: {...obj.subObj} }; // faz clone. 
+
+             obj2.subObj.test = 456;
+
+            /* const objMerged = {
+                 ...obj,
+                 ...obj2
+             };*/
+
+             console.log(obj2);
 
 
