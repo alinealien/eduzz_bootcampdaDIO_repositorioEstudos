d583a47d3bc1edfaf2ca29a98c0b2365eea2c699
alinealien <alinebdantunes@gmail.com>
2021-11-13 13:01:01 -0300

    Atualizando arquivos da aula fetch, async/Await e EventEmmiter

--- Desenvolvimento Avançado com JavaScript ES6/Aplicando Conceitos Promises e Fetch/aula_II.js
+++ Desenvolvimento Avançado com JavaScript ES6/Aplicando Conceitos Promises e Fetch/aula_II.js
@@ -1,7 +1,13 @@
  //Fetch: Para fazer requesições usando Promises
 
  fetch('/data.json').then(responseStream => {
-    responseStream.json().then(data => {
+     console.log(responseStream);
+ }); 
+
+ /*fetch('/data.json')
+ .then(responseStream => responseStream.json())
+    .then(data => {
         console.log(data);
-    });
- });
+    }).catch(err => {
+
+ });*/
