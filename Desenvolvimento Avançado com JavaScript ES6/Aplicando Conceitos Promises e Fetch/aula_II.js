c8261abc052b4124bf3ec644abc1f51be23a83b0
alinealien <alinebdantunes@gmail.com>
2021-11-13 17:45:00 -0300

    Finalizando aula fetch, async/await e EventEmitter

--- Desenvolvimento Avançado com JavaScript ES6/Aplicando Conceitos Promises e Fetch/aula_II.js
+++ Desenvolvimento Avançado com JavaScript ES6/Aplicando Conceitos Promises e Fetch/aula_II.js
@@ -1,8 +1,11 @@
  //Fetch: Para fazer requesições usando Promises
 
- fetch('/data.json').then(responseStream => {
+/*
+
+
+ /*fetch('/data.json').then(responseStream => {
      console.log(responseStream);
- }); 
+ });*/ 
 
  /*fetch('/data.json')
  .then(responseStream => responseStream.json())
@@ -11,3 +14,29 @@
     }).catch(err => {
 
  });*/
+
+ // ES7 - Async /  Await(Espera a promise resolver. Assincorno e Sequencial)
+
+const asyncTimer = () =>
+  new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve(12345);
+      }, 1000);
+  });
+
+ const simpleFune = async () => {
+ const data = await asyncTimer();
+ console.log(data);
+ const dataJSON = await fetch('/data.json').thrn(resStream.json())
+    //throw new Error('Oh, no!'); 
+    
+    return data;
+ };
+
+ simpleFunc()
+    .then(data => {
+     console.log(data);
+ })
+    .catch(err => {
+     console.log(err);
+ });
