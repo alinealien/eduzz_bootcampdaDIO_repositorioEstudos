7ba0b458ccf0e089301a09d72c8678dbc9629d3f
alinealien <alinebdantunes@gmail.com>
2021-11-18 23:01:30 -0300

    Aula sobre testes, TDD e BDD

---
+++ Desenvolvimento Avançado com JavaScript ES6/Conceitos aplicados a qualidade de codigo e automacao de testes em JS/testes.js
@@ -0,0 +1,53 @@
+// Testes
+
+//Testes Automatizados
+
+/*  Testes unitários;
+    Testes integrados;
+    Testes funcionais;  */
+
+// Testes usabilidade;
+// Testes de aceitação de usuário;
+// Protótipos;
+// Testes funcionais;
+// Exemplos;
+// Alpha e beta;
+// Mias...
+
+// Ferramentas de testes
+
+//testes de carga e performace;
+//testes de segurança;
+//mais...
+
+
+/* TDD (Test Driven Development):ss
+
+ É um dos pilares do Extreme Programing, consiste em testar e refatorar em pequenos ciclos,
+onde você escreve o teste antes do código , faz o mesmo passar e refatorar o código.*/
+
+//Vantagens:
+
+//Feedback rapido;
+//Maior segurança em alterações e novas funcionalidades;
+//Código mais limpo;
+//Produtividade;
+
+
+/*BDD (Behavior Driven Development)
+
+Técnica de desenvolvimento agil que visa integrar regras de negocios com linguagens de programacao; */
+
+//Pilares:
+
+//Testes;
+//Documentação;
+//Exemplos;
+
+
+/* Vantagens
+
+//Compartilhamento de conhecimento;
+//Documentação dinâmica;
+//Visão do todo; */
+
