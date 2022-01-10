19e751771a20a0621a56fb021339316e4d7714ab
alinealien <alinebdantunes@gmail.com>
2022-01-10 16:50:49 -0300

    Atualizando aula sobre Typescript- Types e Interfaces

--- intro-typescript/index.ts
+++ intro-typescript/index.ts
@@ -1,32 +1,59 @@
+//AULA I
 /*function soma(a:number, b:number) {
     return a + b;
 }
 soma(2, 4);*/
  
-// types
-// interfaces
-
+// AULA II - types e interfaces
+// types : juntar diferentes interfaces ou definir que uma var pode ser uma interface ou outra
+// interfaces : definir contratos de estruturas de dados
+/*
 interface IAnimal{
     nome: string;
     tipo: 'terrestre' | 'aquático';
-    executarRugido(alturaEmDecibeis: number) : void;
+    domestico: boolean;
 }
 
 interface IFelino extends IAnimal {
     visaoNoturna: boolean;
 }
 
-const animal : IAnimal = {
-    nome: 'Elefante',
-    tipo: 'terrestre',
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+interface ICanino extends IAnimal {
+    porte: 'pequeno' | 'medio' | 'grande';
+
 }
 
-animal.executarRugido(' ');
+type IDomestico = IFelino | ICanino; // definindo um tipo ou outro/ tbm podemos juntar
+
+const animal : IDomestico = {
+   domestico: true,
+   nome: 'cachorro',
+   porte: 'medio',
+   tipo: 'terrestre',
+   visaoNoturna: true,
+    
+}
+
+/*animal.executarRugido(' ');
 
 const felino : IFelino = {
     nome: 'Leão',
     tipo: 'terrestre',
     visaoNoturna: true,
 
-}
+}*/
+
+//AULA III - tag input
+//aqui com o (as), ele explicita que é um elemento HTML - pode ser feito com qualquer tipo de elemento
+//HTMLInputElement
+
+const input = document.getElementById('input') as HTMLInputElement; 
+
+input.addEventListener('input', (event) => {
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+    
+    //console.log('Me lasquei do coração');
+    //console.log(event.currentTarget.);
+
+});
