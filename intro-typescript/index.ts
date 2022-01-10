1dabe0f84168ed9115af8df47491afc41b92a61e
alinealien <alinebdantunes@gmail.com>
2022-01-10 14:57:31 -0300

    Atualizando aula de funcionamento de interfaces

--- intro-typescript/index.ts
+++ intro-typescript/index.ts
@@ -1,1 +1,32 @@
-console.log('TypeScrit');
+/*function soma(a:number, b:number) {
+    return a + b;
+}
+soma(2, 4);*/
+ 
+// types
+// interfaces
+
+interface IAnimal{
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    executarRugido(alturaEmDecibeis: number) : void;
+}
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+const animal : IAnimal = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+}
+
+animal.executarRugido(' ');
+
+const felino : IFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+
+}
