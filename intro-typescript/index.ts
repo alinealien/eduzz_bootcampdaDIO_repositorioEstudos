3ac03a996473db0c431105ac83d8eedc9eec4afd
alinealien <alinebdantunes@gmail.com>
2022-01-10 21:32:39 -0300

    Finalizando aula sobre TypeScript

--- intro-typescript/index.ts
+++ intro-typescript/index.ts
@@ -46,6 +46,7 @@ const felino : IFelino = {
 //AULA III - tag input
 //aqui com o (as), ele explicita que é um elemento HTML - pode ser feito com qualquer tipo de elemento
 //HTMLInputElement
+/*
 
 const input = document.getElementById('input') as HTMLInputElement; 
 
@@ -56,4 +57,108 @@ input.addEventListener('input', (event) => {
     //console.log('Me lasquei do coração');
     //console.log(event.currentTarget.);
 
+}); */
+
+// AULA IV - Generic types - so aceita tipos iguais
+/*
+
+function adcionarApendiceALista<NaoSei>(array: any[], valor: NaoSei){
+    return array.map(item => item + valor);
+}
+
+adcionarApendiceALista([1, 2, 3], 1); //ele inferiu que o valor é numerico, mesmo acima definindo um tipo generico
+*/
+
+//desenvolvendo condicionais com parametros 
+/* interface IUsuario {
+    id: string;
+    email: string;
+}
+
+interface IAdmin {
+    cargo: 'gerente' | 'coordenador' | 'supervisor';
+}
+
+//utiliza-se o in para verificar se há um item na interface
+function redirecione(usuario: IUsuario | IAdmin){
+    if('cargo' in usuario){
+        //redirecionar para a área de admnistração
+    }
+
+        //redirecionar para a área
+} */
+
+//AULA V - "?" para variaveis opcionais
+/*
+interface IUsuario {
+    id: string;
+    email: string;
+    cargo?: 'gerente' | 'coordenador' | 'supervisor' | 'funcionario';
+    
+    //ao colocar o ? ele tranformou em dado condicional (pode ou nao esta definido)
+}
+
+function redirecione(usuario: IUsuario ){
+    if(usuario.cargo){
+        //redirecionar(usuario.cargo);
+    }
+
+    // redirecionar para a área do usuário
+} */
+
+//AULA VI - Variaveis com propriedade readonly e private
+/*
+interface Cachorro {
+    nome: string;
+    idade: number;
+    parqueFavorito?: string;
+}
+//interando todos os itens que adcionar e os valores serao apenas de leitura na hora da implementacao
+//-? Remova os valores opcionais
+type CachorroSomenteLeitura = {
+    readonly [ K in keyof Cachorro]-?: Cachorro[K];
+}
+
+class MeuCachorro implements Cachorro {
+    idade;
+    nome;
+    parqueFavorito?: string;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+const cao = new MeuCachorro('Sirius', 6);*/
+
+//AULA - IMPORTANDO BIBLIOTECAS COM TYPESCRIPT
+//IMPORTANTE PARA O DESENVOLVIMENTO COM O TS - LEIA ABAIXO -
+//npm i --save-dev @types/jquery - para adcionar as dependecias do jquery
+//npm install -g typescript - para instalar o typescript
+//tsc --init - cria o arquivo TSCONFIG.JSON
+/*
+import $ from 'jquery';
+
+$.fn.extend({
+    novaFuncao(){
+        console.log('Chamou nova funcao');
+    }
 });
+
+$('body').novaFuncao();*/
+
+interface Pessoa {
+    nome: string;
+    idade: number;
+    nacionalidade:string;
+    
+}
+
+interface Brasileiro extends Omit<Pessoa, 'nacionalidade'> {
+
+}
+
+const brasileiro: Brasileiro = {
+    idade 
+}
---
+++ intro-typescript/typings.d.ts
@@ -0,0 +1,6 @@
+/* o que for adcionado aqui, se tiver o mesmo nome dos tipos definidos da biblioteca 
+externa que utilizamos, ele irá somar as duas interfaces e ao fim teremos acesso aos seus itens*/
+
+interface JQuery {
+    novaFuncao(): void;
+}
